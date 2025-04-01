506884389deb4a92c1edcea4ffd33c4715558faf
Diogo Steinke Deconto <diogo.deconto@pf.up.edu.br>
2025-03-31 21:53:58 -0300

    API com Controllers e configuração com o banco de dados

---
+++ Ecommerce/API/Controllers/ProdutoController.cs
@@ -0,0 +1,39 @@
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/produto")]
+public class ProdutoController : ControllerBase
+{
+    private readonly AppDataContext _context;
+    public ProdutoController(AppDataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("cadastrar")]
+    public IActionResult Cadastrar
+        ([FromBody] Produto produto)
+    {
+        _context.Produtos.Add(produto);
+        _context.SaveChanges();
+        return Created("", produto);
+    }
+
+    //Exemplo de método dentro de uma classe
+    [HttpGet]
+    public string Metodo()
+    {
+        return "";
+    }
+
+    //Exemplo de um EndPoint dentro de um Controller
+    [HttpGet("helloworld")]
+    public IActionResult HelloWorld()
+    {
+        return Ok("Hello World!");
+    }
+}
